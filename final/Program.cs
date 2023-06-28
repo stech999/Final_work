cdf1daa28e1b1dc481461719847ec719a18c3d35
Красавчик <stech999@ya.ru>
2023-06-28 16:42:32 +0300

    Написали программу для подсчета количества символов в каждой строке

--- final/Program.cs
+++ final/Program.cs
@@ -2,13 +2,16 @@
 
 Console.Clear();
 
-string[] elements = { $"Hello, 2, world, :-)" };
+string[] elements = { $"Hello", "2", "world", ":-)" };
+// Console.Write($"{elements.Length}");
+// Console.WriteLine();
 PrintArray(elements);
 
 void PrintArray(string[] el)
 {
     for (int i = 0; i < el.Length; i++)
     {
-        Console.Write($"{el[i]}");
+        Console.Write($"Длина элемента ({el[i]}) в массиве равна: ");
+        Console.WriteLine($"{el[i].Length} ");
     }
 }
