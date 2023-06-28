68eb51effc9b2610681d756ec99567ccabc43f54
Красавчик <stech999@ya.ru>
2023-06-28 15:29:50 +0300

    Создан массив для вывода элементов

--- final/Program.cs
+++ final/Program.cs
@@ -2,4 +2,13 @@
 
 Console.Clear();
 
-string[] elements = {$"Hello, 2, world, :-)"};
+string[] elements = { $"Hello, 2, world, :-)" };
+PrintArray(elements);
+
+void PrintArray(string[] el)
+{
+    for (int i = 0; i < el.Length; i++)
+    {
+        Console.Write($"{el[i]}");
+    }
+}
