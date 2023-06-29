ec63d9db5a12b3a8c2ee47ca0e6ab0c5178cdfd1
Красавчик <stech999@ya.ru>
2023-06-29 19:08:25 +0300

    к решению программы добавленны пункты выбора решения задания

--- final/Program.cs
+++ final/Program.cs
@@ -1,29 +1,83 @@
 ﻿// Начало программы
 
 Console.Clear();
-// можно раскомментировать и проверить кажддый вариант:
-string[] elements = { $"Hello", "world", ":-)" }; // первый вариант
-// string[] elements = { $"1234", "1567", "-2", "computer science" }; // второй вариант
-// string[] elements = { $"Russia", "Denmark", "Kazan" }; // третий вариант
 
-PrintArray(elements);
+Console.Write($"Выберите номер примера: \n 1. Hello, 2, world, :-)\n 2. 1234, 1567, - 2, computer science \n 3. Russia, Denmark, Kazan");
+Console.WriteLine("\n");
 
-void PrintArray(string[] el)
+
+Console.Write($"Выберите вариант: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
 {
-    for (int i = 0; i < el.Length; i++)
+    string[] elements = { $"Hello", "2", "world", ":-)" };
+
+    PrintArray(elements);
+
+    void PrintArray(string[] el)
     {
-        // Console.WriteLine($"Длина элемента ({el[i]}) в массиве равна: {el[i].Length}"); // Добавляем проверку работы вывода количества символов
+        string[] newarray = new string[el.Length];
 
-        if (el[i].Length <= 3)
+        for (int i = 0; i < el.Length; i++)
         {
-            Console.WriteLine($" ");
-            Console.WriteLine($"Элемент под номером {i} = ({el[i]}) имеет меньше или равен 3 символам");
-            Console.WriteLine($" ");
+            if (el[i].Length <= 3)
+            {
+                newarray[i] = el[i];
+            }
         }
-
-        else
-        {
-            Console.WriteLine($"Этот элемент ({el[i]}) не подходит под условие так как имеет больше 3 символов");
-        }
+        Console.WriteLine("Итог:");
+        Console.Write($"[{string.Join(" ", newarray)}]");
     }
+
+}
+
+else if (number == 2)
+{
+    string[] elements = { $"1234", "1567", "-2", "computer science" };
+
+    PrintArray(elements);
+
+    void PrintArray(string[] el)
+    {
+        string[] newarray = new string[el.Length];
+
+        for (int i = 0; i < el.Length; i++)
+        {
+            if (el[i].Length <= 3)
+            {
+                newarray[i] = el[i];
+            }
+        }
+
+        Console.WriteLine("Итог:");
+        Console.Write($"[{string.Join(" ", newarray)}] ");
+    }
+}
+
+else if (number == 3)
+{
+    string[] elements = { $"Russia", "Denmark", "Kazan" };
+
+    PrintArray(elements);
+
+    void PrintArray(string[] el)
+    {
+        string[] newarray = new string[el.Length];
+
+        for (int i = 0; i < el.Length; i++)
+        {
+            if (el[i].Length <= 3)
+            {
+                newarray[i] = el[i];
+            }
+        }
+
+        Console.WriteLine("Итог:");
+        Console.Write($"[{string.Join(" ", newarray)}] ");
+    }
+}
+
+else
+{
+    Console.Write("Такого номера нет, выберите один из трех вариантов");
 }
